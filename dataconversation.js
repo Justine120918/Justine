644fd7369485467e34b8ebedf50b7f2db846c855
Justine120918 <camarillocombisjustine@gmail.com>
2024-09-15 07:19:19 +0000

    Initial commit with app.js

---
+++ dataconversation.js
@@ -0,0 +1,27 @@
+// 1. Convert a String to a Number
+let price = "200";
+
+// Method 1: parseInt()
+console.log(parseInt(price));
+
+// Method 2: Number()
+console.log(Number(price));
+
+// Method 3: + operator
+console.log(+price);
+
+// 2. Convert a Number to a String
+let distance = 150;
+
+// Method 1: .toString()
+console.log(distance.toString());
+
+// Method 2: String()
+console.log(String(distance));
+
+// 3. Convert a String to a Decimal Number
+let weight = "72.5";
+
+// Using parseFloat()
+console.log(parseFloat(weight));
+
